862bdabbb1fdbe1d5f174424005493e94bd8b038
Marcelo Wiermann <marcelo@marcelo-GA-880GM-UD2H.(none)>
2012-07-17 20:13:15 -0300

    Cleaned up the testing suite

--- spec/licensario_spec.rb
+++ spec/licensario_spec.rb
@@ -2,31 +2,38 @@ require 'spec_helper'
 
 describe Licensario do
 
-  before(:each) do
-    key = 'db886331e9105fc19dc9fd6df2caebab9f112c3c81877ea3a3bfcfe3076aa77d'
-    secret = '5545981d57eb3244e857d561946f4ee312023000da1b9f6b86ab017ee48e5c2d'
-    @api = Licensario::API.new(key,secret)
+  describe Licensario::User do
   end
 
-  it 'should initialize an API driver' do
-    api = Licensario::API.new('key','secret')
-    api.key.should eq('key')
-    api.secret.should eq('secret')
+  describe Licensario::License do
   end
 
-  it 'checks the API heartbeat' do
+  describe Licensario::LicensedResource do
   end
 
-  it 'Ensures the existence of an external user' do
-    res = @api.ensure_external_user_exists('1', 'some@user.net')
-    (res['body'] =~ /^\s*$/).should eq(0)
-    res['status'].should eq('200')
-  end
+  describe Licensario::API do
+   let(:api) do
+     key = 'db886331e9105fc19dc9fd6df2caebab9f112c3c81877ea3a3bfcfe3076aa77d'
+     secret = '5545981d57eb3244e857d561946f4ee312023000da1b9f6b86ab017ee48e5c2d'
+     Licensario::API.new(key,secret)
+   end
 
-  it 'Retrieves the User Licenses' do
-  end
-
-  it 'Checks if the Resource is available' do
+   # before(:each) do
+ 
+    it 'checks the API heartbeat' do
+    end
+  
+    it 'Ensures the existence of an external user' do
+      res = api.ensure_external_user_exists('1', 'some@user.net')
+      (res['body'] =~ /^\s*$/).should eq(0)
+      res['status'].should eq('200')
+    end
+  
+    it 'Retrieves the User Licenses' do
+    end
+  
+    it 'Checks if the Resource is available' do
+    end
   end
 
 end
